f6ec404fb3f07f5689115c65bbc6d1dafb1240bd
Pranav Teja <86045290+Pranavtej@users.noreply.github.com>
2021-11-21 14:13:10 +0530

    Add files via upload

---
+++ tejbot.py
@@ -0,0 +1,137 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+reflections = {
+  "i am"       : "you are",
+  "i was"      : "you were",
+  "i"          : "you",
+  "i'm"        : "you are",
+  "i'd"        : "you would",
+  "i've"       : "you have",
+  "i'll"       : "you will",
+  "my"         : "your",
+  "you are"    : "I am",
+  "you were"   : "I was",
+  "you've"     : "I have",
+  "you'll"     : "I will",
+  "your"       : "my",
+  "yours"      : "mine",
+  "you"        : "me",
+  "me"         : "you"
+}
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what is your name ?",
+        ["I am a tejbot created by using nltk modules. you can call me crazy!",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing goodnHow about You ?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+       r"fine",
+      ["How much fine can i pay you :(", ]
+        ],
+    [
+        r"I am fine",
+        ["Great to hear that, How can I help you?",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","How can I help you?:)",]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program Seriously you are asking me this?",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+    ],
+    [
+        r"(.*) created ?",
+        ["Pranav created me using Python's NLTK library "]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['in a Cloud',]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ",]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football",]
+    ],
+    [
+      r"who is pranav",
+      ["Pranav is Web developer from Srkr Engineering college", ]
+        ],
+     [
+      r"who is manoj",
+      ["manoj is a Naughty boy krishna with around gopiks", ]
+        ],
+[
+      r"who is satvik",
+      ["satvik topper of our college", ]
+        ],
+
+    [
+        r"who (.*) sportsperson ?",
+        ["Messy","Ronaldo","Roony"]
+    ],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["Brad Pitt"]
+    ],
+    [
+                r"how to create bot ",
+             ["its a very simple thing , there are lots of moduels just surf on google :)", ]
+    ],
+
+    [
+        r"i am looking for online guides and courses to learn data science, can you suggest?",
+        ["Crazy_Tech has many great articles with each step explanation along with code, you can explore"]
+    ],
+[
+      r"bye",
+      ["Bye take care. See you soon :", ]
+        ],
+    [
+        r"quit",
+        ["Bye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+
+
+]
+def chat():
+    print("Hi! I am a tejbot created by Pranav for you")
+    chat = Chat(pairs, reflections)
+    chat.converse()
+#initiate the conversation
+if __name__ == "__main__":
+    chat()
